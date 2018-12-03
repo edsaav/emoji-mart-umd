3b06e5d8168fe1eaa9768c703a8a860098d69265
Etienne Lemay <etienne@heliom.ca>
2018-12-03 17:15:08 -0500

    Wrap in UMD

---
+++ scripts/build-dist.js
@@ -0,0 +1,40 @@
+var path = require('path')
+var extend = require('util')._extend
+var webpack = require('webpack')
+
+var configs = ['development', 'production'].map((env) => {
+  var configPath = '../src/webpack.config.js',
+      config = require(configPath)
+
+  delete require.cache[require.resolve(configPath)]
+
+  if (env === 'production') {
+    config.output.filename = config.output.filename.replace(/\.js$/, '.min.js')
+    config.optimization.minimize = true
+  }
+
+  return config
+})
+
+webpack(configs, (err, stats) => {
+  if (err) throw err
+
+  console.log(
+    stats.toString({
+      colors: true,
+      hash: true,
+      version: false,
+      timings: true,
+      assets: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false,
+      children: false,
+      cached: false,
+      reasons: false,
+      source: false,
+      errorDetails: false,
+      chunkOrigins: false,
+    })
+  )
+})
